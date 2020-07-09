13c7155c81d720d1fa5381002a010ae7ee3b72ba
Andressa Martins <andressamartins@ic.ufal.br>
2020-07-09 14:21:43 -0300

    Mudando fields dos models

--- pages/models.py
+++ pages/models.py
@@ -7,10 +7,10 @@ import math
 import numpy as np
 
 class Amplifier(models.Model):
-    responsible = models.CharField(max_length=200, verbose_name="Responsável")
+    responsible = models.CharField(max_length=200, verbose_name="Responsible")
     email = models.EmailField() 
-    reference = models.CharField(max_length=200,verbose_name="Referência")  
-    mask = models.FileField(upload_to='csv files/', null = True, blank=True, verbose_name="Máscara (.csv)")
+    reference = models.CharField(max_length=200,verbose_name="Reference")  
+    mask = models.FileField(upload_to='csv files/', null = True, blank=True, verbose_name="Mask (.csv)")
     def __str__(self):
         return self.reference
 	
@@ -36,17 +36,17 @@ class Document(models.Model):
         return self.file_info
 
 class Modelo(models.Model):
-    model_type = models.CharField(max_length=255, blank=True, verbose_name="Tipo do Modelo") # RNA ou SVM
-    amplifier = models.ForeignKey(Amplifier, on_delete=models.CASCADE, verbose_name="Amplificador") # 
-    file_h5 = models.FileField(upload_to='h5files/', null = True, blank=True, verbose_name="Modelo NN (.h5)")
-    file_txt = models.FileField(upload_to='txtfiles/', null = True, blank=True, verbose_name="Informação da rede (.txt)")
+    model_type = models.CharField(max_length=255, blank=True, verbose_name="Model Type") # RNA ou SVM
+    amplifier = models.ForeignKey(Amplifier, on_delete=models.CASCADE, verbose_name="Amplifiers") # 
+    file_h5 = models.FileField(upload_to='h5files/', null = True, blank=True, verbose_name="Model NN (.h5)")
+    file_txt = models.FileField(upload_to='txtfiles/', null = True, blank=True, verbose_name="Network information(.txt)")
     def __str__(self):
         return self.amplifier.reference
 
 class RequestPrediction(models.Model):
-    name = models.CharField(max_length=255, blank=True, verbose_name="Título") # RNA ou SVM
-    net_model = models.ForeignKey(Modelo, on_delete=models.CASCADE, verbose_name="Modelo") #
-    pin_signal = models.FileField(upload_to='requests/', null = True, blank=True, verbose_name="Sinais de entrada (Pin)")
+    name = models.CharField(max_length=255, blank=True, verbose_name="Title") # RNA ou SVM
+    net_model = models.ForeignKey(Modelo, on_delete=models.CASCADE, verbose_name="Model") #
+    pin_signal = models.FileField(upload_to='requests/', null = True, blank=True, verbose_name="Input signal (Pin)")
     ganho = models.IntegerField(null=True)
     def __str__(self):
         return self.name
